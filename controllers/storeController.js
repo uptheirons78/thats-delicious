4eddb46df10a95d87bed037c9c48ef4d73e728b4
Mauro <bono.mauro@gmail.com>
2018-04-01 23:28:01 +0200

    add more controllers, add and edit store form

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
     res.render('index', { title: 'I Love Food!'});
 }
@@ -6,6 +9,37 @@ exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store'});
 }
 
-exports.createStore = (req, res) => {
-    res.json(req.body)
+//use async await to avoid "callbacks hell"!
+exports.createStore = async (req, res) => {
+    //let's create a Store and save it
+    const store = await (new Store(req.body)).save();
+    req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+    //redirect to Store page
+    res.redirect(`/store/${store.slug}`);
 }
+
+exports.getStores = async (req, res) => {
+    //1. Query DB for a list of all stores
+    const stores = await Store.find();
+    res.render('stores', {title: 'Stores', stores});
+}
+
+exports.editStore = async (req, res) => {
+    //1. find the store with given id
+    const store = await Store.findOne({ _id: req.params.id });
+    //2. confirm they are owners of the store to edit
+    //todo later
+    //3. render out the edit form
+    res.render('editStore', {title: `Edit ${store.name}`, store});
+}
+
+exports.updateStore = async (req, res) => {
+    // find and update the store
+    const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true, // return the new store instead of the old one
+      runValidators: true
+    }).exec();
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`);
+    res.redirect(`/stores/${store._id}/edit`);
+    // Redriect them the store and tell them it worked
+};
