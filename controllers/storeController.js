c03fd72d05413f82ad7f7ac86dea25505bdc1377
Mauro <bono.mauro@gmail.com>
2018-04-03 22:04:51 +0200

    enable Google API for static map and correct upload store image input

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,86 +1,84 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 const multer = require('multer');
-const jimp = require('jimp'); //to resize photos
-const uuid = require('uuid');//to give photos a unique identifier
+const jimp = require('jimp');
+const uuid = require('uuid');
 
 const multerOptions = {
-    storage: multer.memoryStorage(),
-    fileFilter(req, file, next) {
-        const isPhoto = file.mimetype.startsWidth('image/');
-        if(isPhoto) {
-            next(null, true);
-        } else {
-            next({message: 'Filetype is not allowed!'}, false);
-        }
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if(isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: 'That filetype isn\'t allowed!' }, false);
     }
-}
+  }
+};
 
 exports.homePage = (req, res) => {
-    res.render('index', { title: 'I Love Food!'});
-}
+  res.render('index');
+};
 
 exports.addStore = (req, res) => {
-    res.render('editStore', { title: 'Add Store'});
-}
+  res.render('editStore', { title: 'Add Store' });
+};
 
 exports.upload = multer(multerOptions).single('photo');
 
-exports.resize = async(req, res, next) => {
-    //check if there is no new file to resize
-    if (!req.file) {
-        next(); //skip to the next middleware
-    }
-    const extension = req.file.mimetype.split('/')[1];
-    req.body.photo = `${uuid.v4()}.${extension}`;
-    //now we resize
-    const photo = await jimp.read(req.file.buffer);
-    await photo.resize(800, jimp.AUTO);
-    await photo.write(`./public/uploads/${req.body.photo}`);
-    //once the photo is written in our filesystem, continue with the next middleware
-    next();
-
-}
-
-//use async await to avoid "callbacks hell"!
-exports.createStore = async (req, res) => {
-    //let's create a Store and save it
-    const store = await (new Store(req.body)).save();
-    req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
-    //redirect to Store page
-    res.redirect(`/store/${store.slug}`);
-}
-
-exports.getStores = async (req, res) => {
-    //1. Query DB for a list of all stores
-    const stores = await Store.find();
-    res.render('stores', {title: 'Stores', stores});
-}
-
-exports.editStore = async (req, res) => {
-    //1. find the store with given id
-    const store = await Store.findOne({ _id: req.params.id });
-    //2. confirm they are owners of the store to edit
-    //todo later
-    //3. render out the edit form
-    res.render('editStore', {title: `Edit ${store.name}`, store});
-}
-
-exports.updateStore = async (req, res) => {
-    //set location data to be a point
-    req.body.location.type = 'Point';
-    // find and update the store
-    const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
-      new: true, // return the new store instead of the old one
-      runValidators: true
-    }).exec();
-    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`);
-    res.redirect(`/stores/${store._id}/edit`);
-    // Redriect them the store and tell them it worked
+exports.resize = async (req, res, next) => {
+  // check if there is no new file to resize
+  if (!req.file) {
+    next(); // skip to the next middleware
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  // now we resize
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  // once we have written the photo to our filesystem, keep going!
+  next();
 };
 
+exports.createStore = async (req, res) => {
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  // 1. Query the database for a list of all stores
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store given the ID
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. confirm they are the owner of the store
+  // TODO
+  // 3. Render out the edit form so the user can update their store
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // set the location data to be a point
+  req.body.location.type = 'Point';
+  // find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+  // Redirect them the store and tell them it worked
+};
+
+//AIzaSyDZPZ28S-sjeUBp9X1Sfulj6JwYq54kIg0
 exports.getStoreBySlug = async (req, res, next) => {
-    const store = await Store.findOne({ slug: req.params.slug });
-    if(!store) return next(); //if no store found...run a 404...next on index.js
-    res.render('store', { store, title: store.name });
+  const store = await Store.findOne({ slug: req.params.slug });
+  if (!store) return next(); //it will pass to the next step in index.js, 404 NOT FOUND
+  res.render('store', { store, title: store.name });
 };
--- routes/index.js
+++ routes/index.js
@@ -7,17 +7,21 @@ const { catchErrors } = require('../handlers/errorHandlers');
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
+
 router.post('/add',
     storeController.upload,
     catchErrors(storeController.resize),
     catchErrors(storeController.createStore)
 );
+
 router.post('/add/:id',
     storeController.upload,
     catchErrors(storeController.resize),
     catchErrors(storeController.updateStore)
 );
+
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
-router.get('/stores/:slug', catchErrors(storeController.getStoreBySlug));
+
+router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 module.exports = router;
